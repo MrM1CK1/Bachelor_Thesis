ffe48804aaa3b9b800f229a945d707d92bb7d0ef
MrM1CK1 <132046538+MrM1CK1@users.noreply.github.com>
2024-02-19 16:31:19 +0100

    kod.py

--- kod.py
+++ kod.py
@@ -156,131 +156,18 @@ for date in landsate_date:
     Emmisivity = calc_kod.LSE(b4_l2, ndvi_TIF, VegC, OUT_FOLDER, 'Emiss_' + date)
     LSTemperature = calc_kod.LST(Emmisivity, BrighTemp, b10_l1, OUT_FOLDER, 'LST_' + date)
 
-# Extracting information from the metadata file
-## neexistuje tady zadne rozdeleni mezi L1 a L2, zadny filtr.. nevime na co python saha
-## tady je potreba presne definovat, jestli pracuju s L1 nebo L2, jinak se to bude motat
-
-#for date in landsate_date: #timto se pojistime, ze stale sahame pro soubory ktere patri do stale stejneho datumu
-    #for path in list_of_paths_clipped: #'output/LC08_L2SP_190025_20220627_20220706_02_T1_SR_B5_Clipped.TIF'
-       # ndvi_TIF = calc_kod.NDVI(b4_l2, b5_l2, OUT_FOLDER, 'NDVI_' + date)
-        #VegC = calc_kod.VC(ndvi_TIF, OUT_FOLDER, 'VC_' + date)
-
+    TOA_refle_b2 = calc_kod.TOA_Reflectance(b2_l2, reflectance_ADD_B2, reflectance_MULT_B2, OUT_FOLDER, 'TOARef_b2_' + date)
+    TOA_refle_b4 = calc_kod.TOA_Reflectance(b4_l2, reflectance_ADD_B4, reflectance_MULT_B4, OUT_FOLDER, 'TOARef_b4_' + date)
+    TOA_refle_b5 = calc_kod.TOA_Reflectance(b5_l2, reflectance_ADD_B5, reflectance_MULT_B5, OUT_FOLDER, 'TOARef_b5_' + date)
+    TOA_refle_b6 = calc_kod.TOA_Reflectance(b6_l2, reflectance_ADD_B6, reflectance_MULT_B6, OUT_FOLDER, 'TOARef_b6_' + date)
+    TOA_refle_b7 = calc_kod.TOA_Reflectance(b7_l2, reflectance_ADD_B7, reflectance_MULT_B7, OUT_FOLDER, 'TOARef_b7_' + date)
+    Albedo_liang = calc_kod.Albedo_liang(TOA_refle_b2, TOA_refle_b4, TOA_refle_b5, TOA_refle_b6, TOA_refle_b7, OUT_FOLDER, 'Albedo_Liang_' + date)
+    Albedo_Tasumi = calc_kod.Albedo_Tasumi(TOA_refle_b2, TOA_refle_b4, TOA_refle_b5, TOA_refle_b7, OUT_FOLDER, 'Albedo_Tasumi_' + date)
 
 
+    #Rn = calc_kod.Rn(Emmisivity, LSTemperature, Albedo_liang, Rsin_value, OUT_FOLDER, 'Rn_' + date)
+    #GroundHeatFlux = calc_kod.GHFlux_1(Rn, ndvi_TIF, OUT_FOLDER, 'GHF_' + date)
+    GroundHeatFlux_2 = calc_kod.GHFlux_2(Albedo_liang,LSTemperature, ndvi_TIF,TOA_radiance_B10_L1, OUT_FOLDER, 'GHF_2_' + date)
+    #Gr = calc_kod.Gr(Rn, OUT_FOLDER, 'G_' + date)
 
-
-"""TOA_refle_b4 = calc_kod.TOA_Reflectance(b4, reflectance_ADD_B4, reflectance_MULT_B4, OUT_FOLDER, 'TOARef_b4_' + dates)
-TOA_refle_b2 = calc_kod.TOA_Reflectance(b2_l2, reflectance_ADD_B2, reflectance_MULT_B2, OUT_FOLDER, 'TOARef_b2_' + date)
-TOA_refle_b5 = calc_kod.TOA_Reflectance(b5, reflectance_ADD_B5, reflectance_MULT_B5, OUT_FOLDER, 'TOARef_b5_' + dates)
-TOA_refle_b6 = calc_kod.TOA_Reflectance(b6, reflectance_ADD_B6, reflectance_MULT_B6, OUT_FOLDER, 'TOARef_b6_' + dates)
-TOA_refle_b7 = calc_kod.TOA_Reflectance(b7, reflectance_ADD_B7, reflectance_MULT_B7, OUT_FOLDER, 'TOARef_b7_' + dates)
-Albedo_liang = calc_kod.Albedo_liang(TOA_refle_b2, TOA_refle_b4, TOA_refle_b5, TOA_refle_b6, TOA_refle_b7, OUT_FOLDER, 'Albedo_L8_Liang_' + dates)
-Albedo_Tasumi = calc_kod.Albedo_Tasumi(TOA_refle_b2, TOA_refle_b4, TOA_refle_b5, TOA_refle_b7, OUT_FOLDER, 'Albedo_L8_Tasumi_' + dates)
-
-
-
-TOA_radiance_B10 = calc_kod.TOA_Radiance(b10, radiance_ADD_B10, radiance_MULT_B10, OUT_FOLDER, 'TOA_Radiance_B10_' + dates)
-
-ndvi_TIF = calc_kod.NDVI(b4, b5, OUT_FOLDER, 'NDVI_' + dates)
-
-#LST_b10 = calc_kod.LST(TOA_radiance_B10, K1_CONSTANT_BAND_10_L8, K2_CONSTANT_BAND_10_L8, OUT_FOLDER, 'LST_L8_B10_' + dates)
-
-VegC = calc_kod.VC(ndvi_TIF, OUT_FOLDER, 'VC_' + dates)
-#Emis = calc_kod.Emissivity(ndvi_TIF_L8,LST_b10_L8, OUT_FOLDER, 'Emissivity_L8_' + dates)
-#BrighTemp = calc_kod.Brightness_Temperature(TOA_radiance_B10, Emis_L8, OUT_FOLDER, 'BrightTemp_L8_' + dates)
-
-
-
-#Finding the Landsat images (Landsat Level 2)
-for landsat_path in landsat_tif:
-    L9_metadata = {}
-    landsat_name = os.path.basename(landsat_path)
-    if 'L2SP' in landsat_name:
-        date = landsat_name.split('_')[3]
-        if date in L9_dict:
-            L9_dict[date].append(landsat_path)
-        else:
-            L9_dict[date] = [landsat_path]      
-
-# Extracting the metadata file
-for dates, list_of_paths in L9_dict.items():
-    list_of_paths_clipped = []
-    txt_path = ''
-    for path in list_of_paths_clipped:
-        list_of_paths.append(path)
-
-    for path in list_of_paths:
-        if 'B1.TIF' in path:
-            txt_path = path.replace('B1.TIF', 'MTL.txt')
-            metadata_file = open(txt_path, 'r')
-            for line in metadata_file:
-                if '=' in line:
-                    key, value = line.strip().split(' = ')
-                    L9_metadata[key] = value                                 
-            metadata_file.close()
-            break 
-
-    # Clipping the images
-    for path in list_of_paths:
-        image_name = os.path.basename(path).replace('.TIF', '')
-        out_path = CLIPPED + '\\' + image_name + '_Clipped.TIF'
-        calc_kod.Raster_clip(path, out_path, SQUARE)
-
-        list_of_paths_clipped.append(out_path) 
-
-    # Extracting information from the metadata file
-    for path in list_of_paths_clipped:
-        if 'B1_' in path:
-            b1_2 = path
-        if 'B2' in path:
-            b2_2 = path
-            reflectance_MULT_B2_2 = float((L9_metadata['REFLECTANCE_MULT_BAND_2']))
-            reflectance_ADD_B2_2 = float((L9_metadata['REFLECTANCE_ADD_BAND_2']))
-        if 'B3' in path:
-            b3_2 = path
-        if 'B4' in path:
-            b4_2 = path
-            reflectance_MULT_B4_2 = float((L9_metadata['REFLECTANCE_MULT_BAND_4']))
-            reflectance_ADD_B4_2 = float((L9_metadata['REFLECTANCE_ADD_BAND_4']))
-        if 'B5' in path:
-            b5_2 = path
-            reflectance_MULT_B5_2 = float((L9_metadata['REFLECTANCE_MULT_BAND_5']))
-            reflectance_ADD_B5_2 = float((L9_metadata['REFLECTANCE_ADD_BAND_5']))
-        if 'B6' in path:
-            b6_2 = path
-            reflectance_MULT_B6_2 = float((L9_metadata['REFLECTANCE_MULT_BAND_6']))
-            reflectance_ADD_B6_2 = float((L9_metadata['REFLECTANCE_ADD_BAND_6']))
-        if 'B7' in path:
-            b7_2 = path
-            reflectance_MULT_B7_2 = float((L9_metadata['REFLECTANCE_MULT_BAND_7']))
-            reflectance_ADD_B7_2 = float((L9_metadata['REFLECTANCE_ADD_BAND_7']))
-        if 'B10' in path:
-            b10_2 = path
-            radiance_MULT_B10_2 = float((L9_metadata['RADIANCE_MULT_BAND_10']))
-            radiance_ADD_B10_2 = float((L9_metadata['RADIANCE_ADD_BAND_10']))
-            K1_CONSTANT_BAND_10 = float((L9_metadata['K1_CONSTANT_BAND_10']))
-            K2_CONSTANT_BAND_10 = float((L9_metadata['K2_CONSTANT_BAND_10']))
-
-
-#    TOA_refle_b2_2 = calc_kod.TOA_Reflectance(b2_2, reflectance_ADD_B2_2, reflectance_MULT_B2_2, OUT_FOLDER, 'TOARef_b2_' + dates)
-#    TOA_refle_b4_2 = calc_kod.TOA_Reflectance(b4_2, reflectance_ADD_B4_2, reflectance_MULT_B4_2, OUT_FOLDER, 'TOARef_b4_' + dates)
-#    TOA_refle_b5_2 = calc_kod.TOA_Reflectance(b5_2, reflectance_ADD_B5_2, reflectance_MULT_B5_2, OUT_FOLDER, 'TOARef_b5_' + dates)
- #   TOA_refle_b6_2 = calc_kod.TOA_Reflectance(b6_2, reflectance_ADD_B6_2, reflectance_MULT_B6_2, OUT_FOLDER, 'TOARef_b6_' + dates)
-  #  TOA_refle_b7_2 = calc_kod.TOA_Reflectance(b7_2, reflectance_ADD_B7_2, reflectance_MULT_B7_2, OUT_FOLDER, 'TOARef_b7_' + dates)
-    #Albedo_liang = calc_kod.Albedo_liang(TOA_refle_b2, TOA_refle_b4, TOA_refle_b5, TOA_refle_b6, TOA_refle_b7, OUT_FOLDER, 'Albedo_L9_Liang_' + dates)
-    #Albedo_Tasumi = calc_kod.Albedo_Tasumi(TOA_refle_b2, TOA_refle_b4, TOA_refle_b5, TOA_refle_b7, OUT_FOLDER, 'Albedo_L9_Tasumi_' + dates)
-
-
-    TOA_radiance_B10 = calc_kod.TOA_Radiance(b10_2, radiance_ADD_B10_2, radiance_MULT_B10_2, OUT_FOLDER, 'TOA_Radiance_L9_B10_' + dates)
-
-    ndvi_TIF = calc_kod.NDVI(b4_2, b5_2, OUT_FOLDER, 'NDVI_' + dates)
-
-    LST_b10 = calc_kod.LST(TOA_radiance_B10, K1_CONSTANT_BAND_10,K2_CONSTANT_BAND_10,  OUT_FOLDER, 'LST_L9_B10_' + dates)
-
-#    VegC = calc_kod.VC(ndvi_TIF, OUT_FOLDER, 'VC_' + dates)
-    Emis = calc_kod.Emissivity(ndvi_TIF,LST_b10, OUT_FOLDER, 'Emis_L9_' + dates)
-    BrighTemp = calc_kod.Brightness_Temperature(TOA_radiance_B10, Emis, OUT_FOLDER, 'BrightTemp_L9_' + dates)
-
-
-pprint.pprint('All done and in order :')
-"""
+pprint('All done and in order')
